a017b90389b6dc6f48251b1fc9abbeb8f9f66dde
DavonHicks123 <47585618+DavonHicks123@users.noreply.github.com>
2019-03-15 23:14:37 -0400

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>list=new ArrayList<Integer>();
+
+		
+		list.add(10);
+		list.add(13);
+		list.add(2);
+		list.add(8);
+		list.add(7);
+		list.add(90);
+		list.add(-1);
+		list.add(2);
+		list.add(4);
+		list.add(5);
+		System.out.println(list.toString());
+		reverse4(list);
+	}
+
+	private static void reverse4(ArrayList<Integer> list) {
+	    System.out.println("Before sorted list:"+list);
+		Collections.sort(Reverselist);
+		System.out.println("After unsorted list:"+list);
+			
+		}
+		
+			
+		
+		
+	}
+
+
