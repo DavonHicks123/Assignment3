dfdf2301de330c8441e3f99d2378d7a9154cc979
DavonHicks123 <47585618+DavonHicks123@users.noreply.github.com>
2019-03-15 23:31:49 -0400

    Add files via upload

--- Assignment3.java
+++ Assignment3.java
@@ -2,35 +2,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Assignment3 {
-
 	public static void main(String[] args) {
-		ArrayList<Integer>list=new ArrayList<Integer>();
-
-		
-		list.add(10);
-		list.add(13);
-		list.add(2);
-		list.add(8);
-		list.add(7);
-		list.add(90);
-		list.add(-1);
-		list.add(2);
-		list.add(4);
-		list.add(5);
-		System.out.println(list.toString());
-		reverse4(list);
-	}
-
-	private static void reverse4(ArrayList<Integer> list) {
-	    System.out.println("Before sorted list:"+list);
-		Collections.sort(Reverselist);
-		System.out.println("After unsorted list:"+list);
+	    ArrayList<Integer>list=new ArrayList<Integer>();
+	    
 			
+			list.add(10);
+			list.add(13);
+			list.add(2);
+			list.add(8);
+			list.add(7);
+			list.add(90);
+			list.add(-1);
+			list.add(2);
+			list.add(4);
+			list.add(5);
+			System.out.println(list.toString());
+			reverse4(list);
+			System.out.println(list);
 		}
 		
-			
-		
-		
-	}
 
-
+		private static void reverse4(ArrayList<Integer> list) {
+		  list.remove(0); list.add(3,10);
+		  list.remove(0); list.add(2,13);
+		  list.remove(0); list.add(1,2);
+		  list.remove(8); list.add(4,2);
+		  list.remove(7); list.add(5,-1);
+		  list.remove(7); list.add(6,90);
+		  list.remove(8); list.add(8,4);
+		  
+		
+		}
+}
